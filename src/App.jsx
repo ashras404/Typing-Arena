55d7e6ecec437a9d6df99fe40973ad04df0bd9eb
Ashras khan <ashraskhan.404@gmail.com>
2026-04-27 12:04:02 +0530

    mobile issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useMemo, useRef } from 'react';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from "react";
 import { Analytics } from '@vercel/analytics/react';
 import { TypingArea } from './components/typing/TypingArea';
 import { Home } from './pages/Home';
